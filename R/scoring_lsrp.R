3966116cab2be0ed46effef8d8c0d465b2f825fb
maffotto <antoniomaffei88@gmail.com>
2018-03-19 18:17:52 +0100

    Fixed Documentation

--- R/scoring_lsrp.R
+++ R/scoring_lsrp.R
@@ -1,46 +1,46 @@
 #' Scoring of the Levenson Self-Report Psychopathy Scale
-#' 
+#'
 #' Function to automatically score the Levenson Self-Report Psychopathy Scale
-#' 
+#'
 #' @author Antonio Maffei
 #' @importFrom magrittr %>%
-#' 
-#' @usage 
+#'
+#' @usage
 #' scoring_lsrp(input_lsrp = x)
 #' scoring_lsrp(input_lsrp = x, indices = c(1,26))
-#' 
-#' @param input_lsrp Input data. It must be a rectangular matrix with column indicating items and rows 
+#'
+#' @param input_lsrp Input data. It must be a rectangular matrix with column indicating items and rows
 #'                   indicating subjects.
-#' @param indices Two values vector indicating the first and the last item in the input data, 
-#'                in order to extract MPQ items from a larger dataframe.
+#' @param indices Two values vector indicating the first and the last item in the input data,
+#'                in order to extract LRSP items from a larger dataframe.
 #'
 #' @return A dataframe with 2 columns representing the subscales from the Levenson Self Report Psychopathy Scale
-#' 
+#'
 #' @export
-#' 
+#'
 scoring_lsrp <- function(input_lsrp,indices = NULL){
-  
+
   if (dim(input_lsrp)[1] < 2){ stop("The input data must have at least two rows")}
-  
+
   if (is.null(indices)){
     raw_resp <- input_lsrp
   } else {
     raw_resp <- input_lsrp[,indices[1]:indices[2]]
   }
-  
+
   # item correction #
   item_to_correct_lsrp <- c(5,11,14,17,19,23,24)
   lsrp_raw_corrected <- raw_resp
   lsrp_raw_corrected[,item_to_correct_lsrp] <- 5-raw_resp[,item_to_correct_lsrp]
-  
+
   # subscale identification and scoring #
   Factor_1_index <- c(1,4,6,7,9:15,17,19,20,22,24)
   Factor_2_index <- c(2,3,5,8,16,18,21,23,25,26)
-  
+
   Factor_1 <- lsrp_raw_corrected[,Factor_1_index] %>% rowSums()
   Factor_2 <- lsrp_raw_corrected[,Factor_2_index] %>% rowSums()
-  
+
   lsrp_scored <- tibble::data_frame(Factor_1,Factor_2)
-  
+
   return(lsrp_scored)
 }
--- R/scoring_staiT.R
+++ R/scoring_staiT.R
@@ -1,40 +1,40 @@
 #' Scoring the STAI-Y1 (Trait version)
-#' 
+#'
 #' Function to automatically score the STAI-Y1
-#' 
+#'
 #' @author Antonio Maffei
 #' @importFrom magrittr %>%
-#' 
-#' @usage 
+#'
+#' @usage
 #' scoring_staiT(input_staiT = x)
 #' scoring_staiT(input_staiT = x, indices = c(1,20))
-#' 
-#' @param input_staiT Input data. It must be a rectangular matrix with column indicating items and rows 
+#'
+#' @param input_staiT Input data. It must be a rectangular matrix with column indicating items and rows
 #'                   indicating subjects.
-#' @param indices Two values vector indicating the first and the last item in the input data, 
-#'                in order to extract MPQ items from a larger dataframe.
+#' @param indices Two values vector indicating the first and the last item in the input data,
+#'                in order to extract STAI-Y1 items from a larger dataframe.
 #'
 #' @return A dataframe with 1 columns representing the scores for STAI Trait version
-#' 
+#'
 #' @export
-#' 
+#'
 scoring_staiT <- function(input_staiT,indices = NULL){
-  
+
   if (dim(input_staiT)[1] < 2){ stop("The input data must have at least two rows")}
-  
+
   if (is.null(indices)){
     raw_resp <- input_staiT
   } else {
     raw_resp <- input_staiT[,indices[1]:indices[2]]
   }
-  
+
   item_standard_stait <- c(2,5,8,9,11,12,15,17,18,20)
   item_inverted_stait <- seq(1:20)[-item_standard_stait]
-  
+
   stait_corrected <- raw_resp
   stait_corrected[,item_inverted_stait] <- 5 - stait_corrected[,item_inverted_stait]
-  
+
   STAI_T <- rowSums(stait_corrected) %>% tibble::as_data_frame()
-  
+
   return(STAI_T)
 }
